6a8a93e99c909b2196329c74314f8765e0900aaf
kahasan <kahraman.h@outlook.com>
2020-04-23 11:22:15 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,17 +18,18 @@ const MOVIE_API_URL = lastSearch
 @inject('store')
 @observer
 class App extends React.Component {
-  componentDidMount() {
-    fetch(MOVIE_API_URL)
-      .then((response) => response.json())
-      .then((jsonResponse) => {
-        this.props.store.movies = jsonResponse.Search;
-        this.props.store.loading = false;
-      });
-  }
+  // componentDidMount() {
+  //   fetch(MOVIE_API_URL)
+  //     .then((response) => response.json())
+  //     .then((jsonResponse) => {
+  //       this.props.store.movies = jsonResponse.Search;
+  //       this.props.store.loading = false;
+  //     });
+  // }
 
   render() {
     let movies2 = [];
+    console.log(process.env.API_KEY);
     if (typeof window !== 'undefined') {
       movies2 = localStorage.getItem('favMovies');
       movies2 = movies2 ? JSON.parse(movies2) : [];
